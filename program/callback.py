d2487d9ef702cc95ef98719b102361ab49788a80
somyajeet100 <89988691+somyajeet100@users.noreply.github.com>
2021-11-21 12:30:28 +0530

    Update callback.py

--- program/callback.py
+++ program/callback.py
@@ -12,7 +12,7 @@ from config import (
 @Client.on_callback_query(filters.regex("cbstart"))
 async def cbstart(_, query: CallbackQuery):
     await query.edit_message_text(
-      f"""✨ **Welcome !! How are uh dear [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
+      f"""🔥 **Welcome !! How are uh dear [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
       ‼️ **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music and video on groups through the new Telegram's VC !! 😎🤘🏻**
       
       

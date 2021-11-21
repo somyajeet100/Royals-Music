e5c5da410f01039ade23b61b11f3485378432e06
somyajeet100 <89988691+somyajeet100@users.noreply.github.com>
2021-11-21 12:32:53 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -50,3 +50,42 @@ async def _human_time_duration(seconds):
 @Client.on_message(
     command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
 )
+async def cbstart(_, query: CallbackQuery):
+    await query.edit_message_text(
+      f"""🔥 **Welcome !! How are uh dear [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
+      ‼️ **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music and video on groups through the new Telegram's VC !! 😎🤘🏻**
+      
+      
+      🤞🏻 **Uh can Find out all commands just click commands button there !!**
+     🔊 **Developed By [Dev](t.me/somyajeet_mishra) 😎🤘 Thanks!!**
+     reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "➕ Group par Add karo ➕",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ],
+                [InlineKeyboardButton("🔺 Basic Guide", callback_data="cbhowtouse")],
+                [
+                    InlineKeyboardButton("🔺 Commands", callback_data="cbcmds"),
+                    InlineKeyboardButton("🔺 Owner", url=f"https://t.me/{OWNER_NAME}"),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🔺 Official Group", url=f"https://t.me/{GROUP_SUPPORT}"
+                    ),
+                    InlineKeyboardButton(
+                        "🔺 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "📀 Source Code", url="https://github.com/somyajeet100/Royals-Music"
+                    )
+                ],
+            ]
+        ),
+            disable_web_page_preview=True,
+    )
+
